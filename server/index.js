d47c8bfb413d7c7ad4abb33b4dc21b4869683c71
maxjacobson <max.jacobson@torchlms.com>
2018-12-15 10:55:45 -0700

    point express to build folder

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors    = require('cors');
 const app     = express();
 
 app.use(cors());
+app.use( express.static( `${__dirname}/../client/build` ) );
+
 
 app.get('/representatives/:state',
   findRepresentativesByState,
