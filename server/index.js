b442b1b5be264e19cf19ebdc3a1561eeb3f2469a
maxjacobson <max.jacobson@torchlms.com>
2018-12-15 10:57:43 -0700

    Correct filepath for express server

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const cors    = require('cors');
 const app     = express();
 
 app.use(cors());
-app.use( express.static( `${__dirname}/../client/build` ) );
-
+app.use( express.static(`${__dirname}/../../client/build`));
 
 app.get('/representatives/:state',
   findRepresentativesByState,
